1f01fd5838b405474711714dd08171cb1c8f51af
ps <pio.szo@gmial.com>
2023-11-30 17:33:54 +0100

    Add printig for output lists

--- cards.py
+++ cards.py
@@ -17,26 +17,31 @@ class Card:
         return f"Kontaktuję się z: {self.name} {self.surname}; {self.email}"
         # definiuję dynamiczny atrybut do zwracania długości imienia i nazwiska
     @property
+    #jak wywołać? TODO 
     def full_name_lengh(self):
         return len(f"{self.name} {self.surname}")
 
 five_fake_cards = [Card(name=faker.first_name(), surname=faker.last_name(), company=faker.company(), occupation=faker.job(), email=faker.company_email()) for i in range(5)]
-print(five_fake_cards[0])
-
-def list_basic_info(list):
-    for item in list:
-        print(f"{item.name} {item.surname}, {item.email}")
 
 by_name = sorted(five_fake_cards, key=lambda item: item.name)
 by_surname = sorted(five_fake_cards, key=lambda item: item.surname)
 by_email = sorted(five_fake_cards, key=lambda item: item.email)
 
-list_basic_info(by_surname)
-contact(by_surname[0])
+def list_basic_info(list):
+    for item in list:
+        print(f"{item.name} {item.surname}, {item.email}")
+#list_basic_info(by_name)
 
-'''
+# list comprehension
+#[print(name) for name in by_name]
+
+# using * and sep parameter to print out list
+#print(*by_surname, sep="\n")
+
+
 def list_full_info(list):
     for item in list:
         print(f"{item.name} {item.surname} pracuje w {item.company} jako {item.occupation}, email: {item.email}")
-list_full_info(five_fake_cards)
-'''
+#list_full_info(five_fake_cards)
+
+#by_name[0].full_name_lengh()
