a674031666ec6d889e3a6aded8e47ec86617cd09
piotrszostak <70960533+piotrszostak@users.noreply.github.com>
2023-12-04 13:00:51 +0100

    Create subclasses of Card

--- cards.py
+++ cards.py
@@ -15,11 +15,31 @@ class Card:
         # dodaję metodę contact() 
     def contact(self):
         return f"Kontaktuję się z: {self.name} {self.surname}; {self.email}"
-        # definiuję dynamiczny atrybut do zwracania długości imienia i nazwiska
-    @property
+    # definiuję dynamiczny atrybut do zwracania długości imienia i nazwiska
     #jak wywołać? TODO 
-    def full_name_lengh(self):
+    @property
+    def label_lengh(self):
         return len(f"{self.name} {self.surname}")
+
+class BaseContact(Card):
+    def __init__(self, priv_number, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.priv_number = priv_number
+    def contact(self):
+        return f"Kontaktuję się z {self.name} {self.surname} pod numerem telefonu {self.priv_number}"
+        
+class BussinessContact(Card):
+    def __init__(self, bussiness_number, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.priv_number = bussiness_number
+    def contact(self):
+        return f"Kontaktuję się z {self.name} {self.surname} pod numerem telefonu {self.bussiness_number}"
+
+def create_contacts(card_type, card_quantity):
+    if card_type == BaseContact():
+        return [BaseContact(name=faker.first_name(), surname=faker.last_name(), priv_number=faker.phone_number()) for i in range(card_quantity)]
+    else:
+        return [BussinessContact(name=faker.first_name(), surname=faker.last_name(), company=faker.company(), occupation=faker.job(), email=faker.company_email(), bussiness_number=faker.phone_number()) for i in range(card_quantity)]
 
 five_fake_cards = [Card(name=faker.first_name(), surname=faker.last_name(), company=faker.company(), occupation=faker.job(), email=faker.company_email()) for i in range(5)]
 
@@ -37,11 +57,5 @@ def list_basic_info(list):
 
 # using * and sep parameter to print out list
 #print(*by_surname, sep="\n")
-
-
-def list_full_info(list):
-    for item in list:
-        print(f"{item.name} {item.surname} pracuje w {item.company} jako {item.occupation}, email: {item.email}")
-#list_full_info(five_fake_cards)
 
 #by_name[0].full_name_lengh()
